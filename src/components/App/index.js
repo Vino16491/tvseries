320f2a86e20f2afbc650cfdd74f6fd51e93660cf
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-19 20:51:01 +0530

    restructured folder structure

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import Intro from '../Intro/App.js'
+
+class App extends Component {
+state={
+  series:[]
+}
+
+componentDidMount(){
+
+  const series = ["Vikings", "Game of Thrones"];
+
+  setTimeout(()=>{
+    this.setState({series: series});
+  }, 2000)
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series List</h1>
+        </header>
+       <Intro message="Here You can find all of your most loved seried" />
+       The length of series array-{this.state.series.length}
+      </div>
+    );
+  }
+}
+
+export default App;
