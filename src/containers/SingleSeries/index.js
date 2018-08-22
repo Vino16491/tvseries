0f7b25379d81ee7631c1722ae746bf2295e9b05f
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-22 22:51:10 +0530

    single series component updated with images

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
-
+import Loader from '../../components/Loader'
 class SingleSeries extends Component{
+
+    state = {
+        show:null
+    }
+
+    componentDidMount(){
+        const {id} = this.props.match.params;
+        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+            .then(response => response.json())
+            .then(json => this.setState({show:json}));
+    }
+
     render(){
+        const {show} = this.state;
+        console.log(show)
         return(
             <div>
-                <p>Single Series</p>
+                {(show === null)? <Loader/> : 
+                    <div>
+                        <p>{show.name !== null && show.name}</p>
+                        <p>{show.premiered !== null && ('Premiered -' + show.premiered) }</p>
+                       {(show.rating.average !== null) && <p>Rating - {show.rating.average}</p> } 
+                        <p>{show._embedded.episodes.length !== 0 && 'Episodes - ' + show._embedded.episodes.length}</p>
+                      { show.image.medium !== '' && <img src={show.image.medium}></img>}
+                    </div>
+                }
             </div>
         )
     }
