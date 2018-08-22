a1bd6b978330b980f465ee6071b5a16f2768760f
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-22 20:26:13 +0530

    fetching tv series dynamically

--- src/containers/series/index.js
+++ src/containers/series/index.js
@@ -3,21 +3,47 @@ import SeriesList from '../../components/SeriesList'
 
 class Series extends Component {
     state = {
-        series: []
+        series: [],
+        seriesName: '',
+        isfetching:false
     };
 
-    componentDidMount() {
-        fetch("http://api.tvmaze.com/search/shows?q=Vikings")
+    onSeriesInputChange = e =>{
+        this.setState({seriesName: e.target.value, isfetching:true});
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
             .then(response => response.json())
             .then(json => this.setState({
-                series: json
+                series: json,
+                isfetching:false
             }));
     }
-    render() {
 
+    render() {
+        const {series, seriesName, isfetching} = this.state;
         return ( 
-            <div> The length of series array- {this.state.series.length}
-            <SeriesList list={this.state.series} />
+            <div>
+            
+                <div>
+                    <input value={seriesName} type='text' onChange={this.onSeriesInputChange}/>
+                </div>
+                {
+                    series.length === 0 && seriesName.trim() === '' 
+                    &&
+                    <p>Please enter series name into the input</p>
+                }
+
+                {
+                    series.length === 0 && seriesName.trim() !== '' 
+                    &&
+                    <p>No TV series have been found with this name</p>
+                }
+                {
+                    isfetching && <p>loading...</p>
+                }
+                {
+                    !isfetching &&  <SeriesList list={this.state.series} />
+                }
+           
             </div>
         )
     }
